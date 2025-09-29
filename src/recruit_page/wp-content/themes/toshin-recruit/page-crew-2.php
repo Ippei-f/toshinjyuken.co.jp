7d31726ae9c2f19b569d2b49afab757b82eeeeed
IPPEI FUKUTA <fukuta@libesta.jp>
2025-09-29 18:19:44 +0900

    update

---
+++ src/recruit_page/wp-content/themes/toshin-recruit/page-crew-2.php
@@ -0,0 +1,100 @@
+<div id="Contents">
+    <div class="kv kv-2025">
+        <div class="h1_copy">
+            <strong>遙かなる旅路を<br class="pc">ともにする仲間たち</strong>
+            <div><img src="[gtdu]/images/crew/h1_en.svg" alt="Our crew" width="756" height="140"></div>
+            <h1>社員紹介</h1>
+        </div>
+    </div>
+    <ul class="topicpath">
+        <li><a href="[home_url]/">TOP</a></li>
+        <li>社員紹介</li>
+    </ul>
+    <div class="wrapper">
+        <section class="intro front">
+            <div class="intro_img"><img src="[gtdu]/images/crew/h2_en.svg" alt="We can do it!"></div>
+            <h2>ともに未来を描こう</h2>
+            <p>東新住建が手がけるのは、物件だけではない。<br>住まう人の未来、その街の未来、そして、ともに働く仲間の未来。<br>時に荒波に飲まれそうになっても、力を合わせて乗り越える。<br>東新住建で働く日々を通じて、かけがえのない未来を描く先輩たちに<br>この会社を選んだ理由、仕事の醍醐味などをインタビューしました。</p>
+        </section>
+        <section class="crew_wrap">
+            <article>
+                <div class="crew_img step01"><a href="[home_url]/interview-01/"><img src="[gtdu]/images/crew/crew01.jpg" alt="受け継がれる仕事の仕組みで1年目から活躍できる"></a></div>
+                <div class="txt_wrap step02">
+                    <h3>受け継がれる仕事の仕組みで<br>1年目から活躍できる</h3>
+                    <p>分譲住宅事業　営業　2021年入社</p>
+                    <a class="txt_btn" href="[home_url]/interview-01/">Read more</a>
+                </div>
+            </article>
+            <!--<article>
+		<div class="crew_img step01"><a href="[home_url]/interview-02/"><img src="[gtdu]/images/crew/crew02.jpg" alt="仲間に支えられながら設計した家でお客さまの暮らしは続いていく"></a></div>
+		<div class="txt_wrap step02">
+			<h3>仲間に支えられながら設計した家で<br>お客さまの暮らしは続いていく</h3>
+			<p>生産管理　設計　2020年入社</p>
+			<a class="txt_btn" href="[home_url]/interview-02/">Read more</a>
+		</div>
+	</article>-->
+        </section>
+
+        <section class="crew-ilikeit">
+            <h2 class="crew-ilikeit__ttl">
+                <img class="pc" src="[gtdu]/images/crew/i_like_it_ttl.svg" alt="I like it!">
+                <img class="sp" src="[gtdu]/images/crew/i_like_it_ttl-sp.svg" alt="I like it!">
+            </h2>
+            <ul class="crew-ilikeit__list">
+                <li class="crew-ilikeit__item">
+                    <img src="[gtdu]/images/crew/i_like_it-1.jpg" alt="実力主義だから努力が報われる！">
+                </li>
+                <li class="crew-ilikeit__item">
+                    <img src="[gtdu]/images/crew/i_like_it-2.jpg" alt="ライフステージが変わっても続けやすい！">
+                </li>
+                <li class="crew-ilikeit__item">
+                    <img src="[gtdu]/images/crew/i_like_it-3.jpg" alt="若手もベテランもフラットに話せる！">
+                </li>
+                <li class="crew-ilikeit__item">
+                    <img src="[gtdu]/images/crew/i_like_it-4.jpg" alt="他では見たことがないものを作ってる！">
+                </li>
+                <li class="crew-ilikeit__item">
+                    <img src="[gtdu]/images/crew/i_like_it-5.jpg" alt="一緒に頑張れる仲間がいる！">
+                </li>
+                <li class="crew-ilikeit__item">
+                    <img src="[gtdu]/images/crew/i_like_it-6.jpg" alt="失敗しても挑戦を歓迎してくれる！">
+                </li>
+                <li class="crew-ilikeit__item">
+                    <img src="[gtdu]/images/crew/i_like_it-7.jpg" alt="成長のチャンスがたくさんある！">
+                </li>
+                <li class="crew-ilikeit__item">
+                    <img src="[gtdu]/images/crew/i_like_it-8.jpg" alt="実績がキャリアに直結する！">
+                </li>
+            </ul>
+        </section>
+
+        <section class="hero max_area" data-overdirection="full">
+            <div class="wrapper">
+                <div class="intro front">
+                    <h2><img src="[gtdu]/images/crew/movie_ttl.svg" alt="東新住建に集うヒーロー社員"></h2>
+                    <p>心に広がる、仕事への熱い思い。<br>語りだせば止まらないけれど…ここはあえて10秒で！<br>たった10秒、されど10秒。<br>思いをぎゅっと詰め込んで、どんな一言が飛び出すのか？！<br>先輩たちの笑顔にも注目です。</p>
+                </div>
+                <div class="action front"><img src="[gtdu]/images/crew/action.svg" alt="action"></div>
+
+                <ul>
+                    <li class="front"><a href="[gtdu]/images/crew/movie/movie08.mp4" class="video-open movie_popup"><img src="[gtdu]/images/crew/hero08.jpg" alt="分譲住宅事業　営業">
+                            <p>分譲住宅事業　営業<br><span>2021 年入社</span></p>
+                        </a></li>
+
+
+                    <li class="front"><a href="[gtdu]/images/crew/movie/movie11.mp4" class="video-open movie_popup"><img src="[gtdu]/images/crew/hero11.jpg" alt="経営管理　広告宣伝">
+                            <p>分譲住宅事業 マーケティング<br><span>2022 年入社</span></p>
+                        </a></li>
+
+                    <li class="front"><a href="[gtdu]/images/crew/movie/movie12.mp4" class="video-open movie_popup"><img src="[gtdu]/images/crew/hero12.jpg" alt="生産管理　工事">
+                            <p>生産管理　工事<br><span>2017 年入社</span></p>
+                        </a></li>
+
+                    <li class="front"><a href="[gtdu]/images/crew/movie/movie14.mp4" class="video-open movie_popup"><img src="[gtdu]/images/crew/hero14.jpg" alt="生産管理　管理">
+                            <p>生産管理　管理<br><span>2020 年入社</span></p>
+                        </a></li>
+                </ul>
+
+            </div>
+        </section>
+    </div>
---
+++ src/recruit_page/wp-content/themes/toshin-recruit/page-form-2.php
@@ -0,0 +1,19 @@
+<div id="Contents">
+    <div class="kv">
+        <div class="h1_copy">
+            <div><img src="[gtdu]/images/form/h1_en.svg" alt="form" width="311" height="107"></div>
+            <h1>エントリーフォーム</h1>
+        </div>
+    </div>
+    <ul class="topicpath">
+        <li><a href="[home_url]/">TOP</a></li>
+        <li>エントリーフォーム</li>
+    </ul>
+    <div class="wrapper">
+        <div class="progressbar">
+            <div class="item active"><span>STEP1</span>必要事項の入力</div>
+            <div class="item"><span>STEP2</span>入力内容の確認</div>
+            <div class="item"><span>STEP3</span>送信完了</div>
+        </div>
+        <p>※お問い合わせの際には、個人情報に関する取扱いへの同意が必要です。<br>文字化けやエラーの原因となりますので、ローマ数字・丸付き数字などの機種依存文字、半角カタカナ（半角文字）は使用しないでください。<br>住所にローマ数字が含まれる場合、アラビア数字や漢数字に置換えて入力してください。<br><span class="red">＊</span>印は必須項目です。</p>
+    </div>
